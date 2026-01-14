564feced3565824a79cc222aa338077b8b6bce4c
tarebor <onodaifetareborgift@gmail.com>
2026-01-14 21:08:09 +0100

    another change

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(session({
     }),
     cookie: {
         httpOnly: true, // client-side JS cannot access cookie
-        secure: process.env.NODE_ENV === 'production', // HTTPS only in prod
+        secure: true, // HTTPS only in prod
         sameSite:'none',
         maxAge: 24 * 60 * 60 * 1000 // 1 day in ms
     }
